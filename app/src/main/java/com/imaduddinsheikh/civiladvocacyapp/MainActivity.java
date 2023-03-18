449cf1762dd4e76c4568ec63a54cefc50a41638f
isheikh8492 <isheikh8492@gmail.com>
2023-03-18 15:11:16 -0500

    added location

--- app/src/main/java/com/imaduddinsheikh/civiladvocacyapp/MainActivity.java
+++ app/src/main/java/com/imaduddinsheikh/civiladvocacyapp/MainActivity.java
@@ -2,18 +2,42 @@ package com.imaduddinsheikh.civiladvocacyapp;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
+    private FusedLocationProviderClient mFusedLocationClient;
+    private static final int LOCATION_REQUEST = 111;
+    private static String locationString = "Unspecified Location";
+    private TextView locationTxtView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        locationTxtView = findViewById(R.id.locationTxtView);
+        mFusedLocationClient =
+                LocationServices.getFusedLocationProviderClient(this);
+        determineLocation();
     }
 
     @Override
@@ -34,5 +58,64 @@ public class MainActivity extends AppCompatActivity {
         } else {
             return super.onOptionsItemSelected(item);
         }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == LOCATION_REQUEST) {
+            if (permissions[0].equals(Manifest.permission.ACCESS_FINE_LOCATION)) {
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    determineLocation();
+                } else {
+                    locationTxtView.setText(locationString);
+                }
+            }
+        }
+    }
+
+    private void determineLocation() {
+        // Check perm - if not then start the  request and return
+        if (ContextCompat.checkSelfPermission(this,
+                android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, LOCATION_REQUEST);
+            return;
+        }
+        mFusedLocationClient.getLastLocation()
+                .addOnSuccessListener(this, location -> {
+                    // Got last known location. In some situations this can be null.
+                    if (location != null) {
+                        locationString = getPlace(location);
+                    }
+                    locationTxtView.setText(locationString);
+                })
+                .addOnFailureListener(this, e ->
+                        Toast.makeText(this,
+                                "Can't fetch location", Toast.LENGTH_LONG).show());
+    }
+
+    private String getPlace(Location loc) {
+
+        StringBuilder sb = new StringBuilder();
+
+        Geocoder geocoder = new Geocoder(this, Locale.getDefault());
+        List<Address> addresses;
+
+        try {
+            addresses = geocoder.getFromLocation(loc.getLatitude(), loc.getLongitude(), 1);
+            String address = addresses.get(0).getAddressLine(0);
+            sb.append(String.format(
+                    Locale.getDefault(),
+                    "%s",
+                    address));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return sb.toString();
     }
 }
